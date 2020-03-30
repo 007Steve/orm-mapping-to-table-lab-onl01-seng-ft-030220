3b2df57c326172e6865fccb86d83c08d4e5e9946
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2020-03-30 16:39:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ def self.all
 end
 def self.create_table
   sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS student (
+  CREATE TABLE IF NOT EXISTS students (
   id INTEGER PRIMARY KEY,
   name TEXT,
   grade INTEGER
